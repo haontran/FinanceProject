70c870dc9d28d6431e706340e748a168767f9dbb
haontran <164290050+haontran@users.noreply.github.com>
2025-05-11 20:46:45 -0500

    Added Search Button
    Added a simple search button

--- frontend/src/Components/SearchBar/Search.tsx
+++ frontend/src/Components/SearchBar/Search.tsx
@@ -1,17 +1,23 @@
-import React, { useState, JSX } from 'react'
+import React, { useState, JSX, ChangeEvent, SyntheticEvent } from 'react'
 
 type Props = {}
 
 const Search : React.FC<Props>= (props: Props): JSX.Element => {
     const [search, setSearch] = useState<string>("")
 
-    const onClick = (e: any) => {
-        setSearch(e.target.value)
-        console.log(e)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value)
+      console.log(e)
     }
+
+    const onClick = (e: SyntheticEvent) => {
+      console.log(e)
+    }
+
   return (
     <div>
-        <input value={search} onChange={(e) => onClick(e)}></input>
+        <input value={search} onChange={(e) => handleChange(e)}></input>
+        <button onClick={(e) => onClick(e)} />
     </div>
   )
 }
