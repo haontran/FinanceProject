fbfd74c78c129d2b066454931d4c82ac608c2711
haontran <164290050+haontran@users.noreply.github.com>
2025-05-24 21:23:12 -0500

    Small update to API calls

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCompanyProfile } from '../../api'
+import { CompanyProfile } from '../../company'
+import { useParams } from 'react-router-dom'
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+
+  let { ticker } = useParams()
+  const [company, setCompany] = useState<CompanyProfile>()
+
+  useEffect(() =>  {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!)
+      setCompany(result?.data[0])
+    }
+    getProfileInit()
+  }, [])
+
   return (
-    <div>CompanyPage</div>
+    <div>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </div>
   )
 }
 
