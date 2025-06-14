8b708e68b57e6a9ff7ce3725ae3730d397140dd0
haontran <164290050+haontran@users.noreply.github.com>
2025-06-13 21:36:59 -0500

    Creating foundation for DTOs

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a184dc212d65eeefbfdab572101ece705b0299d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82865304b8eaa87f9395971e47c83034e804ce6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
