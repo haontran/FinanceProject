2d99ab4ba74304f6588964157c5f26abcbaa3cd6
haontran <164290050+haontran@users.noreply.github.com>
2025-06-24 19:53:06 -0500

    Small update refactoring to repository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -17,9 +18,30 @@ namespace api.Repository
             _context = context;
 
         }
+
+        public Task<Stock> CreateAsync(Stock stockModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<List<Stock>> GetAllAsync()
         {
             return _context.Stocks.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDTO stockDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
