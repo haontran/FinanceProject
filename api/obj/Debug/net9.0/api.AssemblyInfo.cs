6340eea3adbc4dcdc2908b26f3619cf712f43b87
haontran <164290050+haontran@users.noreply.github.com>
2025-07-10 21:40:23 -0500

    Data Validation

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9531f86e67ea7300c1ef4a437ce311e95ea32e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a286315646838702a2f1577423702bf1d13cc384")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
