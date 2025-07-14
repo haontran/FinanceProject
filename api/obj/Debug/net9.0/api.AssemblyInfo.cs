f4b952769f4e03b710c1fea9ad339cc32d6f64b4
haontran <164290050+haontran@users.noreply.github.com>
2025-07-13 22:32:10 -0500

    Small changes

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2642d6d2dbb2b82eec65b781e3b559e77d4c619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e61dc1cdd0e1c6d9f8f18a1d53adc2b433ae7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
